6c578e21792184a8516c3ddc1f9ded937128371e
MuhammadZain111 <codingsnippetsbymz@gmail.com>
2026-09-07 06:08:28 -0700

    messaging Features addedd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProtectedRoute from '../routes/ProtectedRoute';
 import './App.css';
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/AuthContext.jsx";
 import ChatAppUI from './pages/ChatAppUI';
 import LoginPage from "./pages/LoginPage";
 import NexoraLandingPage from './pages/NexoraLandingPage';
