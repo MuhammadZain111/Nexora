f1a6dcc9703e46b3b18d4f93fa633a4eb6cd09c6
MuhammadZain111 <codingsnippetsbymz@gmail.com>
2026-09-07 08:26:35 -0700

    issues got solved

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 import user from "../store/chatSlice";
 
-const socket = io("http://localhost:3001");
+const socket = io(import.meta.env.SOCKET_URL, {
+  autoConnect: false,
+  transports: ["websocket"],
+});
 
 
 socket.emit("add-user", user._id);
